ebb56d8679d9d3cd66354c1a1e5db989ef5da98c
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-09 06:07:57 -0700

    added solution

---
+++ e-search_insert_position.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            m = (l+r) // 2
+
+            # lower bound
+            if nums[m] > target:
+                r = m-1
+            # upper bound
+            elif nums[m] < target:
+                l = m+1
+            # base case
+            else:
+                return m
+        return l
