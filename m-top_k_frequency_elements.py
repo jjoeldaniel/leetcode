11dfd071179941d9f46e59a648bc0a633c7c9e68
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-06-09 06:20:08 -0700

    updated solution

--- m-top_k_frequency_elements.py
+++ m-top_k_frequency_elements.py
@@ -1,13 +1,10 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = {}
+        d = defaultdict(int)
         res = []
 
         for i in nums:
-            if i in d:
-                d[i] = d[i] + 1
-            else:
-                d[i] = 1
+            d[i] += 1
 
         d = OrderedDict(sorted(d.items(), reverse=True, key=lambda x: x[1]))
         for key in d.keys():
